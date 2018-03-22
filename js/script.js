a79ff536fa2d424d8dfdfe6cec1684fcfaa7b92e
quentin mccarthy <845868401@bun2-206-14.local>
2018-03-23 10:21:38 +1300

    Menu progress made, javascript OK

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
 $(document).ready(function(){
 	$("#navButton").click(function(){
 		$("#slideLeftNav").addClass("slideLeftNavOpen");
-		$("#slideLeftHotspot").fadeIn()
+		$("#slideLeftHotspot").fadeIn();
 	});
 
 	$("#navButtonClose, #slideLeftHotspot").click(function(){
 		$("#slideLeftNav").removeClass("slideLeftNavOpen");
-		$("#slideLeftHotspot").fadeOut()
+		$("#slideLeftHotspot").fadeOut();
 	});
+
+	// MENU TABS
+
+	var menuURL = window.location.href.indexOf("menus.html");
+	var breakfastURL = window.location.href.indexOf("?tab=breakfast");
+	var lunchURL = window.location.href.indexOf("?tab=lunch");
+
+	if(menuURL > 0){
+		if(breakfastURL > 0){
+			$(".menuTabOption").removeClass("activeMenuOption");
+			$("#breakfastTab").addClass("activeMenuOption");
+			$("#drinksMenu").hide();
+			$("#breakfastMenu").show();
+			$("#lunchMenu").hide();
+		} else if(lunchURL > 0){
+			$(".menuTabOption").removeClass("activeMenuOption");
+			$("#lunchTab").addClass("activeMenuOption");
+			$("#drinksMenu").hide();
+			$("#breakfastMenu").hide()
+			$("#lunchMenu").show()
+		} else {
+			$(".menuTabOption").removeClass("activeMenuOption");
+			$("#drinksTab").addClass("activeMenuOption");
+			$("#drinksMenu").show();
+			$("#breakfastMenu").hide()
+			$("#lunchMenu").hide()
+		}
+	}
 });
